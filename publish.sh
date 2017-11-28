ee77249b79238aa1d2069b6c6fdbfe252965c740
Hammad Asif <mrhammadasif@gmail.com>
2017-11-28 21:45:39 +0500

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,4 +1,12 @@
-git checkout dist
-git rebase master
-git push
-git checkout master
+if [ $# -eq 0 ]
+  then
+    git checkout -b dist
+    git rebase master
+    git push origin dist
+    git checkout master
+else
+  git checkout -b dist
+  git rebase ${1}
+  git push origin dist
+  git checkout ${1}
+fi
